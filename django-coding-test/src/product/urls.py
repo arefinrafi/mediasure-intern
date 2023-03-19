cfa97fb8fe1d96e4b90063481be49c5f9d4db1c9
arefinrafi <arefin.rafifiver15@gmail.com>
2023-03-19 23:19:42 +0600

    complete the tasks

--- django-coding-test/src/product/urls.py
+++ django-coding-test/src/product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from product.views.product import CreateProductView
+from product.views.product import CreateProductView, AllProductView
 from product.views.variant import VariantView, VariantCreateView, VariantEditView
 
 app_name = "product"
@@ -10,11 +10,12 @@ urlpatterns = [
     # Variants URLs
     path('variants/', VariantView.as_view(), name='variants'),
     path('variant/create', VariantCreateView.as_view(), name='create.variant'),
-    path('variant/<int:id>/edit', VariantEditView.as_view(), name='update.variant'),
+    path('variant/<int:id>/edit', VariantEditView.as_view(), name='update_variant'),
 
     # Products URLs
     path('create/', CreateProductView.as_view(), name='create.product'),
-    path('list/', TemplateView.as_view(template_name='products/list.html', extra_context={
-        'product': True
-    }), name='list.product'),
+    path('list/', AllProductView.as_view(), name='list.product'),
+    # path('list/', TemplateView.as_view(template_name='products/list.html', extra_context={
+    #     'product': True
+    # }), name='list.product'),
 ]
--- django-coding-test/src/product/views/product.py
+++ django-coding-test/src/product/views/product.py
@@ -1,6 +1,8 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +13,53 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+    
+class AllProductView(generic.TemplateView):
+    model = Product
+    template_name = 'products/list.html'
+    # context_object_name = "product_list"
+    # paginate_by = 2
+    # page_kwarg = 'product'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(AllProductView, self).get_context_data(**kwargs)
+        product = Product.objects.all()
+        # productvariant = ProductVariant.objects.filter(Q(variant__title__icontains = 'Color') & Q(variant_title__gt = 1)).values('variant_title')
+        
+        productvariantcolor = list(set(ProductVariant.objects.filter(variant__title__icontains = 'Color').values_list("variant_title", flat = True)))
+        productvariantsize = list(set(ProductVariant.objects.filter(variant__title__icontains = 'Size').values_list("variant_title", flat = True)))
+        productvariantstyle = list(set(ProductVariant.objects.filter(variant__title__icontains = 'Style').values_list("variant_title", flat = True)))
+
+        paginator = Paginator(product, 3)
+        page = self.request.GET.get('page')
+        paged_products = paginator.get_page(page)
+
+        search = self.request.GET.get('title', None)
+        if search:
+            paged_products = product.filter(title__icontains = search)
+
+        variant = self.request.GET.get('variant', None)
+        if variant:
+            paged_products = product.filter(productvariant__variant_title__icontains = variant)
+
+        if 'price_from' in self.request.GET:    
+            filter_price1 = self.request.GET.get('price_from', None)
+            filter_price2 = self.request.GET.get('price_to', None)
+            if filter_price1 =='':
+                filter_price1=0
+            else:
+                paged_products = Product.objects.filter(productvariantprice__price__range=(filter_price1,filter_price2))
+
+        date = self.request.GET.get('date', None)
+        if date:
+            paged_products = product.filter(created_at__icontains = date)
+
+        context['product'] = paged_products
+        context['productvariantcolor'] = productvariantcolor
+        context['productvariantsize'] = productvariantsize
+        context['productvariantstyle'] = productvariantstyle
+        context['product_count'] = len(product)
+        
         return context
